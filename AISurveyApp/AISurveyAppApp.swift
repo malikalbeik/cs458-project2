763ee3bc26c566808e3f1df7ec4d441d40352b60
Malik Albeik <albeik.malik@gmail.com>
2024-03-24 10:57:33 +0300

    Initial Commit

---
+++ AISurveyApp/AISurveyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AISurveyAppApp.swift
+//  AISurveyApp
+//
+//  Created by Malik Albeik on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AISurveyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AISurveyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AISurveyApp
+//
+//  Created by Malik Albeik on 24/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
